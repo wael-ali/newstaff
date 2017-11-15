76c18fc9c209a372d38b94325be8b7bb1f89fd2f
Wael Ali <wael.ali@yfu.de>
2017-11-15 11:07:40 +0100

    finish layout and routes

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title, "My App NewStaff"
+    assert_equal full_title("Help"), "Help | My App NewStaff"
+  end
+end
