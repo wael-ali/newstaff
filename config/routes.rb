d040ddb1fe6e83c7190f11fb3acbc0299827e999
Wael Ali <wael.ali@yfu.de>
2017-11-16 11:44:04 +0100

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
+  get '/signup', to: 'users#create'
+
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help', as: "helf"
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  resources :users
 end
