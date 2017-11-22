a17db67270b4e94c308b47fa5c5fba11f1d0e145
Wael Ali <wael.ali@yfu.de>
2017-11-22 10:52:47 +0100

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,44 @@
 module SessionsHelper
+
+  def method_name
+
+  end
+  # Remembers a user in a presistent session
+    def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
   # Los in the given user
   def log_in(user)
     session[:user_id] = user.id
   end
   # Return the current logged-user if any
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
+
   def loged_in?
     !current_user.nil?
   end
+
+  # forget a presistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
+
   before_save {self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum:50}
@@ -7,10 +10,36 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false}
   validates :password,presence: true, length: {minimum: 6}
   has_secure_password
-  # Rturns the hash digest of the given string
-  # def User.digest(string)
-  #   cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine.MIN_COST :
-  #                                                 BCrypt::Engine.cost
-  #   BCrypt::password.create(string, cost: cost)
-  # end
+
+  # remembers a user in the database for use in presistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(digested_str)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(digested_str)
+  end
+
+
+
+
+    # Rturns the hash digest of the given string
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    # Returns a random  token
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    # Forgets a user
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
+
+
 end
